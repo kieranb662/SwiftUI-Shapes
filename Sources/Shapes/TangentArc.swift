d9942d69495bcccc1475b6d8ee206644107e656b
Kieran Brown <kb6627500@gmail.com>
2020-07-29 12:41:55 -0400

    Added public initializer

--- Sources/Shapes/TangentArc.swift
+++ Sources/Shapes/TangentArc.swift
@@ -8,11 +8,18 @@
 
 import SwiftUI
 
-struct TangentArc: Shape {
+public struct TangentArc: Shape {
     public var start: CGPoint
     public var tangent1End: CGPoint
     public var tangent2End: CGPoint
     public var radius: CGFloat
+    
+    public init(start: CGPoint, tangent1End: CGPoint, tangent2End: CGPoint, radius: CGFloat) {
+        self.start = start
+        self.tangent1End = tangent1End
+        self.tangent2End = tangent2End
+        self.radius = radius 
+    }
 
     public var animatableData: AnimatablePair<AnimatablePair<CGPoint, CGFloat>, AnimatablePair<CGPoint, CGPoint>> {
         get {AnimatablePair(AnimatablePair(self.start, self.radius), AnimatablePair(self.tangent1End, self.tangent2End))}
@@ -24,7 +31,7 @@ struct TangentArc: Shape {
         }
     }
 
-    func path(in rect: CGRect) -> Path {
+    public func path(in rect: CGRect) -> Path {
         Path { path in
             path.move(to: start)
             path.addArc(tangent1End: tangent1End, tangent2End: tangent2End, radius: radius)
